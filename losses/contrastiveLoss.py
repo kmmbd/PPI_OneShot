9dc24809458277450d38c60d97199d2b89e444f9
Kazi Mahbub <33881094+kmmbd@users.noreply.github.com>
2020-12-01 10:44:59 +0100

    Add files via upload

--- losses/contrastiveLoss.py
+++ losses/contrastiveLoss.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+from scipy.spatial import distance
 
 
 # class ContrastiveLoss(nn.Module):
@@ -30,7 +32,17 @@ class ContrastiveLoss(nn.Module):
         self.eps = 1e-9
 
     def forward(self, output1, output2, target, size_average=True):
+
+        # o1 = output1.cpu().numpy()
+        # o1 = o1.detach().numpy()
+        #
+        # o2 = output2.detach().numpy()
+        # X = np.vstack([o1, o2])
+        # V = np.cov(X.T)
+        # IV = np.linalg.inv(V)
+        # distances = distance.mahalanobis(output1, output2, IV)
         distances = (output2 - output1).pow(2).sum(1)  # squared distances
         losses = 0.5 * (target.float() * distances +
                         (1 + -1 * target).float() * F.relu(self.margin - (distances + self.eps).sqrt()).pow(2))
+        # losses = torch.from_numpy(losses).float().cuda()
         return losses.mean() if size_average else losses.sum()
